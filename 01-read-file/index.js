bc2499065e44cee2dd539c26cd3add32cb6b09d5
Inna T <meeoow005@gmail.com>
2022-11-05 00:43:46 +0400

    feat: solve 1 task

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('node:fs');//импорт модуля работы с файловой системой
+const path = require('node:path'); //импорт модуля для работы с путями к файлам и каталогам
+let normalPath = path.join('text.txt');
+let readStream = fs.createReadStream(normalPath, 'utf-8');
+readStream.on('data', function (chunk) {
+  console.log(chunk);
+});
+
+readStream.on('error', function () {
+  console.log(0);
+});
+

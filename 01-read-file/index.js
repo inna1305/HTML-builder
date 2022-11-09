62f408488e00d1a537243fbb43b34e1447b41b73
Inna T <meeoow005@gmail.com>
2022-11-09 19:18:00 +0400

    fix: fix path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');//импорт модуля работы с файловой системой
 const path = require('node:path'); //импорт модуля для работы с путями к файлам и каталогам
-let normalPath = path.join('text.txt');
+let normalPath = path.join('01-read-file/text.txt');
 let readStream = fs.createReadStream(normalPath, 'utf-8');
 readStream.on('data', function (chunk) {
   console.log(chunk);
